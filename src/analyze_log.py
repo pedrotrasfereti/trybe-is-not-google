b422bc7bdba00d82db403e64e6d1802fbcd9f2c7
Pedro Trasfereti <pedrotrasfereti@gmail.com>
2022-07-28 19:57:06 -0300

    feat: implement analyze_log method

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,6 +1,7 @@
 from csv import DictReader
 
 
+# csv file helpers
 def read_csv_file(path):
     try:
         with open(path, "r") as file:
@@ -17,6 +18,7 @@ def write_to_file(path, content):
         file.write(content)
 
 
+# data helpers
 def get_most_ordered_item_by_customer(data, customer):
     orders = []
 
@@ -32,7 +34,7 @@ def get_most_ordered_item_by_customer(data, customer):
     return most_ordered
 
 
-def get_total_burger_ordered_by_customer(data, customer):
+def get_total_burgers_ordered_by_customer(data, customer):
     hamburgers = 0
 
     for row in data:
@@ -43,24 +45,6 @@ def get_total_burger_ordered_by_customer(data, customer):
             hamburgers += 1
 
     return hamburgers
-
-
-def get_days_never_visited_by_customer(data, customer):
-    days_open = set()
-    visited_days = set()
-
-    for row in data:
-        curr_day = row["dia"]
-        curr_customer = row["cliente"]
-
-        days_open.add(curr_day)
-
-        if curr_customer == customer:
-            visited_days.add(curr_day)
-
-    never_visited = days_open.difference(visited_days)
-
-    return never_visited
 
 
 def get_never_ordered_by_customer(data, customer):
@@ -81,5 +65,38 @@ def get_never_ordered_by_customer(data, customer):
     return never_ordered
 
 
-def analyze_log(path_to_file):
-    raise NotImplementedError
+def get_days_never_visited_by_customer(data, customer):
+    days_open = set()
+    visited_days = set()
+
+    for row in data:
+        curr_day = row["dia"]
+        curr_customer = row["cliente"]
+
+        days_open.add(curr_day)
+
+        if curr_customer == customer:
+            visited_days.add(curr_day)
+
+    never_visited = days_open.difference(visited_days)
+
+    return never_visited
+
+
+def analyze_log(path):
+    data = read_csv_file(path)
+
+    most_ordered_by_maria = get_most_ordered_item_by_customer(data, "maria")
+    total_burgers_by_arnaldo = get_total_burgers_ordered_by_customer(
+        data, "arnaldo")
+    never_ordered_by_joao = get_never_ordered_by_customer(data, "joao")
+    never_visited_by_joao = get_days_never_visited_by_customer(data, "joao")
+
+    content = (
+        f"{most_ordered_by_maria}\n"
+        f"{total_burgers_by_arnaldo}\n"
+        f"{never_ordered_by_joao}\n"
+        f"{never_visited_by_joao}\n"
+    )
+
+    write_to_file("data/mkt_campaign.txt", content)
