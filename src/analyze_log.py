56095cca3654e7ca1c775ad493e346da6ba1d1e0
Pedro Trasfereti <pedrotrasfereti@gmail.com>
2022-07-28 19:08:01 -0300

    feat: create read_csv_file and write_to_file helpers

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,2 +1,21 @@
+from csv import DictReader
+
+
+def read_csv_file(path):
+    try:
+        with open(path, "r") as file:
+            data = DictReader(file, ["cliente", "pedido", "dia"])
+
+            return list(data)
+
+    except FileNotFoundError:
+        raise FileNotFoundError("File not found!")
+
+
+def write_to_file(path, content):
+    with open(path, "w") as file:
+        file.write(content)
+
+
 def analyze_log(path_to_file):
     raise NotImplementedError
